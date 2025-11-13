bb587cb77a4cbee11da22cfccb654dbd734740e9
pc-pupu <pc.pupai@gmail.com>
2025-11-13 18:27:55 +0530

    Code in Dashboard Controller

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\StaticpageController;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\DashboardController;
 
 Route::get('/users', [UserController::class, 'index']);
 
@@ -12,4 +14,7 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', [AuthController::class, 'user']);
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/dashboard', [DashboardController::class, 'index']);
 });
+
+ 
